4e5bb1ab2fe4640cb0f759ec70e4b844082d7e60
David R. Heffelfinger <dheffelfinger@ensode.com>
2017-07-16 12:39:44 -0400

    Renamed "jsf" project to "jsf_intro",changed package name for Customer.java, updated dependencies on jsf_intro to Java EE 8.

---
+++ ch02_src/jsf_intro/src/main/java/net/ensode/glassfishbook/jsf/Customer.java
@@ -0,0 +1,37 @@
+package net.ensode.glassfishbook.jsf;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class Customer {
+
+  private String firstName;
+  private String lastName;
+  private String email;
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+}
