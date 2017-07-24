42b79cce363b9db54a66b886fcf6891251b017d7
David R. Heffelfinger <dheffelfinger@ensode.com>
2017-07-24 17:39:17 -0400

    Changed scope of ArbitraryMessageController.java to @ViewScoped. Reformatted source code.

--- ch02_src/jsfarbitrarymess/src/main/java/net/ensode/javaee8book/jsfarbitrarymess/ArbitraryMessageController.java
+++ ch02_src/jsfarbitrarymess/src/main/java/net/ensode/javaee8book/jsfarbitrarymess/ArbitraryMessageController.java
@@ -1,20 +1,22 @@
 package net.ensode.javaee8book.jsfarbitrarymess;
 
-import javax.enterprise.context.RequestScoped;
+import java.io.Serializable;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 @Named
-@RequestScoped
-public class ArbitraryMessageController {
-    
+@ViewScoped
+public class ArbitraryMessageController implements Serializable {
+
     @Inject
     FacesContext facesContext;
 
     public void saveData() {
-        FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_INFO, "Data saved successfully", "All Data successfully saved.");
+        FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_INFO,
+                "Data saved successfully", "All Data successfully saved.");
         facesContext.addMessage(null, facesMessage);
     }
 
