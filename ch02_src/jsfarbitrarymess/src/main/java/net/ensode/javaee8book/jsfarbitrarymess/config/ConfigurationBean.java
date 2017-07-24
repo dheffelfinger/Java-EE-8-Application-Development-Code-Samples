f80c2bcbb9a01013f00d2837f5efce5a499fded2
David R. Heffelfinger <dheffelfinger@ensode.com>
2017-07-24 15:58:15 -0400

    Added beans.xml and @FacesContext annotated class.

---
+++ ch02_src/jsfarbitrarymess/src/main/java/net/ensode/javaee8book/jsfarbitrarymess/config/ConfigurationBean.java
@@ -0,0 +1,8 @@
+package net.ensode.javaee8book.jsfarbitrarymess.config;
+
+import javax.faces.annotation.FacesConfig;
+
+@FacesConfig(version = FacesConfig.Version.JSF_2_3)
+public class ConfigurationBean {
+
+}
