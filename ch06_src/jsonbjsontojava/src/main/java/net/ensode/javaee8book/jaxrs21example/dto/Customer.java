67e1aa21a668f1a138c6490297caa0bf2b37e342
David R. Heffelfinger <dheffelfinger@ensode.com>
2017-08-26 06:14:14 -0400

    Removed unnecessary dependencies from JSON-B example.

--- ch06_src/jsonbjsontojava/src/main/java/net/ensode/javaee8book/jaxrs21example/dto/Customer.java
+++ ch06_src/jsonbjsontojava/src/main/java/net/ensode/javaee8book/jaxrs21example/dto/Customer.java
@@ -1,21 +1,13 @@
 package net.ensode.javaee8book.jaxrs21example.dto;
 
 import java.time.LocalDate;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.PastOrPresent;
 
 public class Customer {
 
     private String salutation;
-    
-    @NotBlank(message = "first name must not be empty")
     private String firstName;
     private String middleName;
-    
-    @NotBlank(message = "last name must not be empty")
     private String lastName;
-    
-    @PastOrPresent(message = "Date of birth must be in the past")
     private LocalDate dateOfBirth;
 
     public String getSalutation() {
