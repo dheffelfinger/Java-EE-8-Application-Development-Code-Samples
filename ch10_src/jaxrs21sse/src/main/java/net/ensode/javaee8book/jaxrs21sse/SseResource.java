35536244b1793153fd5eb018460ecc2728430e71
David R. Heffelfinger <dheffelfinger@ensode.com>
2017-09-02 13:57:28 -0400

    Added JAX-RS 2.1 SSE example

---
+++ ch10_src/jaxrs21sse/src/main/java/net/ensode/javaee8book/jaxrs21sse/SseResource.java
@@ -0,0 +1,45 @@
+package net.ensode.javaee8book.jaxrs21sse;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.sse.OutboundSseEvent;
+import javax.ws.rs.sse.Sse;
+import javax.ws.rs.sse.SseEventSink;
+
+@Path("serversentevents")
+public class SseResource {
+
+    List<Float> stockTickerValues = null;
+
+    @GET
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    public void sendEvents(@Context SseEventSink sseEventSink, @Context Sse sse) {
+        initializeStockTickerValues();
+        new Thread(() -> {
+            stockTickerValues.forEach(value -> {
+                try {
+                    System.out.println(String.format("Sending the following value: %.2f", value));
+                    final OutboundSseEvent outboundSseEvent = sse.newEventBuilder()
+                            .name("ENSD stock ticker value")
+                            .data(String.class, String.format("%.2f", value))
+                            .build();
+                    sseEventSink.send(outboundSseEvent);
+                    Thread.sleep(5000);
+                } catch (InterruptedException ie) {
+                    ie.printStackTrace();
+                }
+            });
+
+        }).start();
+    }
+
+    private void initializeStockTickerValues() {
+        stockTickerValues = Stream.of(50.3f, 55.5f, 62.3f, 70.7f, 10.1f, 5.1f).collect(Collectors.toList());
+    }
+}
