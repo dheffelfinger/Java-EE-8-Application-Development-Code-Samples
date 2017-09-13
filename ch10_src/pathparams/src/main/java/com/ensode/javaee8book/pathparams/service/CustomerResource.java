ebf0f39964681380cf4e706b6d1f665614fbdd5d
David R. Heffelfinger <dheffelfinger@ensode.com>
2017-09-13 19:41:44 -0400

    Added example demonstrating developing a JAX-RS web service that accepts path parameters.

---
+++ ch10_src/pathparams/src/main/java/com/ensode/javaee8book/pathparams/service/CustomerResource.java
@@ -0,0 +1,82 @@
+package com.ensode.javaee8book.pathparams.service;
+
+import com.ensodejavaee8book.pathparams.entity.Customer;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+@Path("/customer/")
+public class CustomerResource {
+
+  private Customer customer;
+
+  public CustomerResource() {
+    customer = new Customer(1L, "William",
+        "Daniel", "Graystone");
+  }
+
+  @GET
+  @Produces("text/xml")
+  @Path("{id}/")
+  public Customer getCustomer(@PathParam("id") Long id) {
+    //in a "real" RESTful service, we would retrieve data from a database
+    //using the supplied id.
+
+    System.out.println("--- " + this.getClass().getCanonicalName()
+        + ".getCustomer() invoked, path parameter id = " + id);
+
+    return customer;
+  }
+
+  /**
+   * Create a new customer
+   * @param customer XML representation of the customer to create
+   */
+  @PUT
+  @Consumes("text/xml")
+  public void createCustomer(Customer customer) {
+    //in a "real" RESTful service, we would parse the XML
+    //received in the customer XML parameter, then insert
+    //a new row into the database.
+
+    System.out.println("--- " + this.getClass().getCanonicalName()
+        + ".createCustomer() invoked");
+
+    System.out.println("customer = " + customer);
+
+  }
+
+  @POST
+  @Consumes("text/xml")
+  public void updateCustomer(Customer customer) {
+    //in a "real" RESTful service, we would parse the XML
+    //received in the customer XML parameter, then update
+    //a row in the database.
+
+    System.out.println("--- " + this.getClass().getCanonicalName()
+        + ".updateCustomer() invoked");
+
+    System.out.println("customer = " + customer);
+
+    System.out.println("customer= " + customer);
+  }
+
+  @DELETE
+  @Consumes("text/xml")
+  @Path("{id}/")
+  public void deleteCustomer(@PathParam("id") Long id) {
+    //in a "real" RESTful service, we would invoke
+    //a DAO and delete the row in the database with the
+    //primary key passed as the "id" parameter.
+
+    System.out.println("--- " + this.getClass().getCanonicalName()
+        + ".deleteCustomer() invoked, id = " + id);
+
+    System.out.println("customer = " + customer);
+  }
+}
