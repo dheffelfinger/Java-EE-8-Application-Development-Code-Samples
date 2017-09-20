9614ce6cbccde14276d02c85738e37784ed5ae13
David R. Heffelfinger <dheffelfinger@ensode.com>
2017-09-20 19:33:47 -0400

    Added example illustrating JSON-P 1.1 JSON Patch support.

---
+++ ch06_src/jsonpatch/src/main/java/net/ensode/javaee8book/jsonpatch/JsonPatchDemoService.java
@@ -0,0 +1,48 @@
+package net.ensode.javaee8book.jsonpatch;
+
+import java.io.StringReader;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonPatch;
+import javax.json.JsonReader;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("jsonpatch")
+public class JsonPatchDemoService {
+
+    private String jsonString;
+
+    @GET
+    public Response jsonPointerDemo() {
+        initializeJsonString();
+        JsonReader jsonReader = Json.createReader(new StringReader(jsonString));
+        JsonArray jsonArray = jsonReader.readArray();
+        JsonPatch jsonPatch = Json.createPatchBuilder()
+                .replace("/1/dateOfBirth", "1977-01-01")
+                .build();
+        JsonArray modifiedJsonArray =jsonPatch.apply(jsonArray);
+
+        return Response.ok(modifiedJsonArray.toString(), MediaType.APPLICATION_JSON).build();
+    }
+
+    private void initializeJsonString() {
+        jsonString = "[\n"
+                + "  {\n"
+                + "    \"dateOfBirth\": \"1997-01-01\",\n"
+                + "    \"firstName\": \"David\",\n"
+                + "    \"lastName\": \"Delabassee\",\n"
+                + "    \"salutation\": \"Mr\"\n"
+                + "  },\n"
+                + "  {\n"
+                + "    \"dateOfBirth\": \"1997-03-03\",\n"
+                + "    \"firstName\": \"David\",\n"
+                + "    \"lastName\": \"Heffelfinger\",\n"
+                + "    \"middleName\": \"Raymond\",\n"
+                + "    \"salutation\": \"Mr\"\n"
+                + "  }\n"
+                + "]";
+    }
+}
