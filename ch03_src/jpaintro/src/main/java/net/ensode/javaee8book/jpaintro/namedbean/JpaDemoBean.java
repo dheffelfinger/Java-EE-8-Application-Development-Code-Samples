03500aec39f6d1d23a49f1da35f957455828ba49
David R. Heffelfinger <dheffelfinger@ensode.com>
2017-10-09 12:11:52 -0400

    Removed automatically generated comments.

--- ch03_src/jpaintro/src/main/java/net/ensode/javaee8book/jpaintro/namedbean/JpaDemoBean.java
+++ ch03_src/jpaintro/src/main/java/net/ensode/javaee8book/jpaintro/namedbean/JpaDemoBean.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package net.ensode.javaee8book.jpaintro.namedbean;
 
 import javax.annotation.Resource;
