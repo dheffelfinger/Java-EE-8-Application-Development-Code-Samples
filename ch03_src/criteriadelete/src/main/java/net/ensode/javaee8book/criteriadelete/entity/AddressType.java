864d65ce1eece09aaee3be1d31d8bd0918b4d090
David R. Heffelfinger <dheffelfinger@ensode.com>
2017-10-09 12:04:37 -0400

    Updated JPA entities to leverage new JPA 2.2 repeatable annotation support.

--- ch03_src/criteriadelete/src/main/java/net/ensode/javaee8book/criteriadelete/entity/AddressType.java
+++ ch03_src/criteriadelete/src/main/java/net/ensode/javaee8book/criteriadelete/entity/AddressType.java
@@ -11,7 +11,6 @@ import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
