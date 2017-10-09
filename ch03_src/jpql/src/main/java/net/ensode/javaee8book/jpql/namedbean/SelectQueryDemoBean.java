767fc7b88967f3bbf470fcf1fbc850963ffdbfb7
David R. Heffelfinger <dheffelfinger@ensode.com>
2017-10-08 20:42:36 -0400

    Added example illustrating the Java Persistence Query Language (JPQL).

---
+++ ch03_src/jpql/src/main/java/net/ensode/javaee8book/jpql/namedbean/SelectQueryDemoBean.java
@@ -0,0 +1,47 @@
+package net.ensode.javaee8book.jpql.namedbean;
+
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import net.ensode.javaee8book.jpql.entity.UsState;
+
+@Named
+@RequestScoped
+public class SelectQueryDemoBean {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private List<UsState> matchingStatesList;
+
+    public String findStates() {
+        String retVal = "confirmation";
+
+        try {
+            Query query = entityManager
+                    .createQuery(
+                    "SELECT s FROM UsState s WHERE s.usStateNm "
+                    + "LIKE :name");
+
+            query.setParameter("name", "New%");
+            matchingStatesList = query.getResultList();
+        } catch (Exception e) {
+            retVal = "error";
+            e.printStackTrace();
+        }
+
+        return retVal;
+    }
+
+    public List<UsState> getMatchingStatesList() {
+        return matchingStatesList;
+    }
+
+    public void setMatchingStatesList(List<UsState> matchingStatesList) {
+        this.matchingStatesList = matchingStatesList;
+    }
+
+}
