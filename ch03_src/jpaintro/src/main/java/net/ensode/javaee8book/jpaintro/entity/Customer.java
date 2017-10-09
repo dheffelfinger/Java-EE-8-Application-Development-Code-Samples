54ddbb3e18870aa5e2ffb14df54a8c9de53bef5f
David R. Heffelfinger <dheffelfinger@ensode.com>
2017-10-08 20:26:09 -0400

    Added introductory JPA example.

---
+++ ch03_src/jpaintro/src/main/java/net/ensode/javaee8book/jpaintro/entity/Customer.java
@@ -0,0 +1,66 @@
+package net.ensode.javaee8book.jpaintro.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CUSTOMERS")
+public class Customer implements Serializable
+{
+  @Id
+  @Column(name = "CUSTOMER_ID")
+  private Long customerId;
+
+  @Column(name = "FIRST_NAME")
+  private String firstName;
+
+  @Column(name = "LAST_NAME")
+  private String lastName;
+
+  private String email;
+
+  public Long getCustomerId()
+  {
+    return customerId;
+  }
+
+  public void setCustomerId(Long customerId)
+  {
+    this.customerId = customerId;
+  }
+
+  public String getEmail()
+  {
+    return email;
+  }
+
+  public void setEmail(String email)
+  {
+    this.email = email;
+  }
+
+  public String getFirstName()
+  {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName)
+  {
+    this.firstName = firstName;
+  }
+
+  public String getLastName()
+  {
+    return lastName;
+  }
+
+  public void setLastName(String lastName)
+  {
+    this.lastName = lastName;
+  }
+
+}
---
+++ ch03_src/jpaintro/src/main/java/net/ensode/javaee8book/jpaintro/namedbean/JpaDemoBean.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.ensode.javaee8book.jpaintro.namedbean;
+
+import javax.annotation.Resource;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
+import net.ensode.javaee8book.jpaintro.entity.Customer;
+
+/**
+ *
+ * @author heffel
+ */
+@Named
+@RequestScoped
+public class JpaDemoBean {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Resource
+    private UserTransaction userTransaction;
+
+    public String updateDatabase() {
+
+        String retVal = "confirmation";
+
+        Customer customer = new Customer();
+        Customer customer2 = new Customer();
+        Customer customer3;
+
+        customer.setCustomerId(3L);
+        customer.setFirstName("James");
+        customer.setLastName("McKenzie");
+        customer.setEmail("jamesm@notreal.com");
+
+        customer2.setCustomerId(4L);
+        customer2.setFirstName("Charles");
+        customer2.setLastName("Jonson");
+        customer2.setEmail("cjohnson@phony.org");
+
+        try {
+            userTransaction.begin();
+            entityManager.persist(customer);
+            entityManager.persist(customer2);
+            customer3 = entityManager.find(Customer.class, 4L);
+            customer3.setLastName("Johnson");
+            entityManager.persist(customer3);
+            entityManager.remove(customer);
+
+            userTransaction.commit();
+        } catch (HeuristicMixedException |
+                HeuristicRollbackException |
+                IllegalStateException |
+                NotSupportedException |
+                RollbackException |
+                SecurityException |
+                SystemException e) {
+            retVal = "error";
+            e.printStackTrace();
+        }
+
+        return retVal;
+    }
+}
