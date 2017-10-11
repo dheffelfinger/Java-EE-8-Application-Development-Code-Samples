b52a66868884673f24137a307984c714411e0f29
David R. Heffelfinger <dheffelfinger@ensode.com>
2017-10-10 20:01:22 -0400

    Updated CDI named beans example.

---
+++ ch05_src/cdinamedbeans/src/main/java/net/ensode/javaee8book/cdidependencyinjection/beans/CustomerController.java
@@ -0,0 +1,25 @@
+package net.ensode.javaee8book.cdidependencyinjection.beans;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class CustomerController {
+
+  private static final Logger logger = Logger.getLogger(
+      CustomerController.class.getName());
+  @Inject
+  private Customer customer;
+
+  public String saveCustomer() {
+
+    logger.log(Level.INFO, "Saving the following information \n{0}", customer.
+            toString());
+
+    return "confirmation";
+  }
+}
