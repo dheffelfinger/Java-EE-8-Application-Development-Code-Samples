6943d17c1c2b778f5081d73da39de981ed6c4867
David R. Heffelfinger <dheffelfinger@ensode.com>
2017-10-20 18:18:17 -0400

    Updated package name.

--- ch04_src/asynchronousmethodsclient/src/main/java/net/ensode/javaee8book/asynchronousmethodsclient/App.java
+++ ch04_src/asynchronousmethodsclient/src/main/java/net/ensode/javaee8book/asynchronousmethodsclient/App.java
@@ -5,7 +5,7 @@ import java.util.concurrent.Future;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
-import net.ensode.glassfishbook.asynchronousmethods.AsynchronousSessionBeanRemote;
+import net.ensode.javaee8book.asynchronousmethods.AsynchronousSessionBeanRemote;
 
 public class App {
 
