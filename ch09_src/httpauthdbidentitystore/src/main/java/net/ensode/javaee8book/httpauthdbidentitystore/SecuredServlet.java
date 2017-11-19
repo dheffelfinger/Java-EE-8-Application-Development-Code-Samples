ac1d695bbd7d6005bff9ea5859f7437e41203fb9
David R. Heffelfinger <dheffelfinger@ensode.com>
2017-11-19 18:44:14 -0500

    Added example illustrating HTTP authorization using a Database Identity store.

---
+++ ch09_src/httpauthdbidentitystore/src/main/java/net/ensode/javaee8book/httpauthdbidentitystore/SecuredServlet.java
@@ -0,0 +1,46 @@
+package net.ensode.javaee8book.httpauthdbidentitystore;
+
+import java.io.IOException;
+import javax.annotation.security.DeclareRoles;
+import javax.security.enterprise.authentication.mechanism.http.FormAuthenticationMechanismDefinition;
+import javax.security.enterprise.authentication.mechanism.http.LoginToContinue;
+import javax.security.enterprise.identitystore.DatabaseIdentityStoreDefinition;
+import javax.security.enterprise.identitystore.Pbkdf2PasswordHash;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.ServletSecurity;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@FormAuthenticationMechanismDefinition(
+        loginToContinue = @LoginToContinue(
+                loginPage = "/login.html",
+                errorPage = "/loginerror.html"
+        )
+)
+
+@DatabaseIdentityStoreDefinition(
+        dataSourceLookup = "java:global/MyDS2",
+        callerQuery = "select password from users where USERNAME = ?",
+        groupsQuery = "select g.GROUP_NAME from USER_GROUPS ug, users u, GROUPS g where ug.USER_ID = u.user_id and g.GROUP_ID= ug.GROUP_ID and u.USERNAME=?",
+        hashAlgorithm = Pbkdf2PasswordHash.class,
+        hashAlgorithmParameters = {
+            "Pbkdf2PasswordHash.Iterations=3072",
+            "Pbkdf2PasswordHash.Algorithm=PBKDF2WithHmacSHA512",
+            "Pbkdf2PasswordHash.SaltSizeBytes=64"
+        }
+)
+@DeclareRoles({"user", "admin"})
+@WebServlet("/securedServlet")
+@ServletSecurity(
+        @HttpConstraint(rolesAllowed = {"admin"}))
+public class SecuredServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.getWriter().write("Congratulations, login successful.");
+    }
+}
